04736254819d7d4d872a3118adcbf3260c8c21cc
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-12-24 15:58:05 +0300

    add layout and login forms

---
+++ components/profile/user-profile.js
@@ -0,0 +1,13 @@
+import ProfileForm from './profile-form';
+import classes from './user-profile.module.css';
+
+function UserProfile() {
+  return (
+    <section className={classes.profile}>
+      <h1>Your User Profile</h1>
+      <ProfileForm />
+    </section>
+  );
+}
+
+export default UserProfile;
---
+++ components/starting-page/starting-page.js
@@ -0,0 +1,11 @@
+import classes from './starting-page.module.css';
+
+function StartingPageContent() {
+  return (
+    <section className={classes.starting}>
+      <h1>Welcome!</h1>
+    </section>
+  );
+}
+
+export default StartingPageContent;
