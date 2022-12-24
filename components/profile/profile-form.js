51ff887889894fb50dc9224d40e9c868e215dea9
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-12-24 22:43:47 +0300

    protect auth page and add change password

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,31 @@
+import { useRef } from 'react';
 import classes from './profile-form.module.css';
 
-function ProfileForm() {
+function ProfileForm(props) {
+  const oldPasswdRef = useRef();
+  const newPasswdRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredOldPasswd = oldPasswdRef.current.value;
+    const enteredNewPasswd = newPasswdRef.current.value;
+
+    props.onChangePassword({
+      oldPassword: enteredOldPasswd,
+      newPassword: enteredNewPasswd,
+    });
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newPasswdRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input type="password" id="old-password" ref={oldPasswdRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
