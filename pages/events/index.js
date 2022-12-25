358aacbdce63a210438a95cf0710c006a690f490
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-12-25 19:16:13 +0300

    list all events and protect page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,28 @@
+import { getAllEvents } from '../../dummy-events-data';
+import EventList from '../../components/events/event-list';
+import { getSession } from 'next-auth/react';
+
 function AllEventsPage() {
-  return <div>All Events</div>;
+  const allEvents = getAllEvents();
+
+  return <EventList items={allEvents} />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
 
 export default AllEventsPage;
