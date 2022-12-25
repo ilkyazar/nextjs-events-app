a57609f18386a85c57682cbb718f1e2e5e8600a9
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-12-25 14:36:41 +0300

    update styles

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,6 @@
-import Link from 'next/link';
+import Button from '../ui/button';
+
+import classes from './event-item.module.css';
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -16,20 +18,20 @@ function EventItem(props) {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + image} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
-          <Link href={exploreLink}>Explore Event</Link>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>Explore Event</Button>
         </div>
       </div>
     </li>
