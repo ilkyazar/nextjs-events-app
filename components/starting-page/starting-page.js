049faa306ee535fa0a40c36f025b60eb83ab3b27
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-12-25 13:21:42 +0300

    add button component

--- components/starting-page/starting-page.js
+++ components/starting-page/starting-page.js
@@ -1,9 +1,11 @@
+import Button from '../ui/button';
 import classes from './starting-page.module.css';
 
 function StartingPageContent() {
   return (
     <section className={classes.starting}>
       <h1>Welcome!</h1>
+      <Button>Click me</Button>
     </section>
   );
 }
